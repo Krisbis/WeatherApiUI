bdc98b013b56484ce6dc02b9a3384ed597b38f12
Kristian <kristian.sikanen@hotmail.com>
2023-04-19 17:14:20 +0300

    windspeed ready

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# WeatherApiUI
-Fetches and displays weather information, which is provided by weather station we built. Serves as an final project on course "IOT &amp; Web-development"."
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-8d59dc4de5201274e310e4c54b9627a8934c3b88527886e3b421487c677d23eb.svg)](https://classroom.github.com/a/D_drWJKh)
+# Final Project
+IoT and Web Development
+
+Follow the project instructions in Dropbox.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const lastFiftyAPI =
 const tempTwentyAPI =
   "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature";
 const winSpdTwentyAPI =
-  "http://webapi19sa-1.course.tamk.cloud/v1/weather/latest/wind_speed";
+  "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_speed";
 
 async function fetchAPI() {
   try {
@@ -27,67 +27,145 @@ async function fetchLastfifty() {
 }
 
 async function fetchTempTwenty(table) {
-    try {
-      const response = await fetch(tempTwentyAPI);
-      const apiData = await response.json();
-  
-      // Extract the temperature values from the API data
-      const temperatures = apiData.slice(-20).map(data => data.temperature);
-      const times = apiData.slice(-20).map(data => data.date_time);
-  
-      // Create the Chart.js chart
-      const canvas = document.getElementById('myChart');
-      const chart = new Chart(canvas, {
-        type: 'line',
-        data: {
-          labels: [...Array(20).keys()].map(i => `Reading ${i+1}`),
-          datasets: [{
-            label: 'Temperature',
-            data: temperatures,
-            backgroundColor: 'cyan',
-            borderColor: "cyan",
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-
-      // Create the table of information
-      const tableHead = table.querySelector("thead");
-      const tableBody = table.querySelector("tbody");
-      tableHead.innerHTML = '<tr><th scope="col">#</th><th scope="col">Time</th><th scope="col">Temperature</th></tr>'
-      tableBody.innerHTML = "";
-      for (let i = 1; i < times.length; i++) {
-        const t = times[i];
-        const c = temperatures[i];
-        const cellElement = document.createElement("tr");
-        cellElement.innerHTML = '<td scope="row">' + i + '</td><td>'+ convertTime(t) +'</td><td>'+ c + ' °C' +'</td>'
-        tableBody.appendChild(cellElement);
-      }
-      
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  window.addEventListener('load', fetchTempTwenty(document.querySelector("table")))
-
-
-async function fetchWinSpdTwenty() {
   try {
-    const response = await fetch(winSpdTwentyAPI);
+    const response = await fetch(tempTwentyAPI);
     const apiData = await response.json();
-    console.log(apiData);
+
+    // Extract the temperature values from the API data
+    const temperatures = apiData.slice(-20).map((data) => data.temperature);
+    const times = apiData.slice(-20).map((data) => data.date_time);
+
+    // Create the Chart.js chart
+    const canvas = document.getElementById("myChart");
+    const chart = new Chart(canvas, {
+      type: "line",
+      data: {
+        labels: [...Array(20).keys()].map((i) => `Reading ${i + 1}`),
+        datasets: [
+          {
+            label: "Temperature",
+            data: temperatures,
+            backgroundColor: "cyan",
+            borderColor: "cyan",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    // Create the table of information
+    const tableHead = table.querySelector("thead");
+    const tableBody = table.querySelector("tbody");
+    tableHead.innerHTML =
+      '<tr><th scope="col">#</th><th scope="col">Time</th><th scope="col">Temperature</th></tr>';
+    tableBody.innerHTML = "";
+    for (let i = 1; i < times.length; i++) {
+      const t = times[i];
+      const c = temperatures[i];
+      const cellElement = document.createElement("tr");
+      cellElement.innerHTML =
+        '<td scope="row">' +
+        i +
+        "</td><td>" +
+        convertTime(t) +
+        "</td><td>" +
+        c +
+        " °C" +
+        "</td>";
+      tableBody.appendChild(cellElement);
+    }
   } catch (error) {
     console.log(error);
   }
+}
+// Conditional call for the function - could've done it inside
+// the code block, but this seemed to bring more clarity to code
+if (window.location.pathname === "/temperature.html") {
+  window.addEventListener(
+    "load",
+    fetchTempTwenty(document.querySelector("table"))
+  );
+}
+
+async function fetchWinSpdTwenty(table) {
+  try {
+    const response = await fetch(winSpdTwentyAPI);
+    const apiData = await response.json();
+
+    // Extract the temperature values from the API data
+    const windspeeds = apiData.slice(-20).map((data) => data.wind_speed);
+    const times = apiData.slice(-20).map((data) => data.date_time);
+
+    // Create the Chart.js chart
+    const canvas = document.getElementById("myChart");
+    const chart = new Chart(canvas, {
+      type: "line",
+      data: {
+        labels: [...Array(20).keys()].map((i) => `Reading ${i + 1}`),
+        datasets: [
+          {
+            label: "Windspeed",
+            data: windspeeds,
+            backgroundColor: "cyan",
+            borderColor: "cyan",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    // Create the table of information
+    const tableHead = table.querySelector("thead");
+    const tableBody = table.querySelector("tbody");
+    tableHead.innerHTML =
+      '<tr><th scope="col">#</th><th scope="col">Time</th><th scope="col">Windspeed</th></tr>';
+    tableBody.innerHTML = "";
+    for (let i = 1; i < times.length; i++) {
+      const t = times[i];
+      const c = windspeeds[i];
+      const cellElement = document.createElement("tr");
+      cellElement.innerHTML =
+        '<td scope="row">' +
+        i +
+        "</td><td>" +
+        convertTime(t) +
+        "</td><td>" +
+        c +
+        " m/s" +
+        "</td>";
+      tableBody.appendChild(cellElement);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+if (window.location.pathname === "/windspeed.html") {
+  window.addEventListener(
+    "load",
+    fetchWinSpdTwenty(document.querySelector("table"))
+  );
 }
 
 // This was formulated by chatGPT,
@@ -95,16 +173,28 @@ async function fetchWinSpdTwenty() {
 // what this function stands for.
 function convertTime(time) {
   const date = new Date(time);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear().toString();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const formattedTime = `${hours}:${minutes} - ${day}.${month}.${year}  `;
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const formattedTime = `${hours}:${minutes}  /  ${day}.${month}.${year}  `;
   return formattedTime;
 }
 
-function autoRefresh() {
-    window.location = window.location.href;
+// Get the current page path
+const currentPagePath = window.location.pathname;
+
+// Loop through all the links and add the "active" class to the link that corresponds to the current page
+const links = document.querySelectorAll("nav a");
+for (const link of links) {
+  if (link.getAttribute("href") === currentPagePath) {
+    link.classList.add("active");
+    break;
+  }
 }
-setInterval('autoRefresh()', 500000);
+
+function autoRefresh() {
+  window.location = window.location.href;
+}
+setInterval("autoRefresh()", 500000);
